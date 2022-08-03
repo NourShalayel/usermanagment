5733516c22c164d9347367802764f46b617b85fa
noor Shalayel <nooresam2001@gmail.com>
2022-08-03 13:54:59 +0300

    up

---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Routes
+router.get('/', userController.view);
+router.post('/', userController.find);
+router.get('/:id', userController.delete);
+
+
+router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
+router.get('/edituser/:id' , userController.edit)
+router.post('/edituser/:id' , userController.update)
+
+router.get('/viewuser/:id' , userController.viewall)
+module.exports = router;
